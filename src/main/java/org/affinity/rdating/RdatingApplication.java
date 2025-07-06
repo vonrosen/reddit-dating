4a20feba2c7af0181db580a928b8dc441180102b
Hunter <hunter@Hunters-MBP-2.attlocal.net>
2025-07-05 17:48:35 -0700

    init

---
+++ src/main/java/org/affinity/rdating/RdatingApplication.java
@@ -0,0 +1,13 @@
+package org.affinity.rdating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdatingApplication.class, args);
+	}
+
+}
