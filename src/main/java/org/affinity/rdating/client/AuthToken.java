aec6fb993650b437a039a9e6ee403ae0838f8424
Hunter <hunter@Hunters-MBP-2.attlocal.net>
2025-07-20 17:53:09 -0700

    more changes

--- src/main/java/org/affinity/rdating/client/AuthToken.java
+++ src/main/java/org/affinity/rdating/client/AuthToken.java
@@ -3,10 +3,10 @@ package org.affinity.rdating.client;
 import java.time.Instant;
 
 public record AuthToken(String accessToken, Instant expires) {
-    private static final long BUFFER_BEFORE_EXPIRATION_MILLIS = 60_1000L;
+    private static final long BUFFER_BEFORE_EXPIRATION_SECONDS = 60L;
 
     boolean expired(){
-        return expires.isBefore(Instant.now().minusSeconds(BUFFER_BEFORE_EXPIRATION_MILLIS));
+        return expires.isBefore(Instant.now().minusSeconds(BUFFER_BEFORE_EXPIRATION_SECONDS));
     }
 
 }
