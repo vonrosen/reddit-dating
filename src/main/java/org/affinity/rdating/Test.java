b95c5384aea613a976fac6ae9550fc1bbd9f369f
Hunter <hunter@Hunters-MBP-2.attlocal.net>
2025-07-29 10:42:03 -0700

    more changes

--- src/main/java/org/affinity/rdating/Test.java
+++ src/main/java/org/affinity/rdating/Test.java
@@ -22,26 +22,26 @@ public class Test {
         String credentials = Base64.getEncoder().encodeToString(String.format("%s:%s", clientId, secret).getBytes(StandardCharsets.UTF_8));
 
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.reddit.com/api/v1/access_token"))
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .header("User-Agent", "rdating/0.1 (by /u/Food-Little)")
-                .header("Authorization", String.format("Basic %s", credentials))
-                .POST(HttpRequest.BodyPublishers.ofString(String.format("grant_type=password&username=%s&password=%s", user, password)))
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String responseBody = response.body();
-        // Parse JSON response to extract access_token
-        String accessToken = null;
-        try {
-            JsonNode jsonNode =
-                new ObjectMapper().readTree(responseBody);
-            accessToken = jsonNode.get("access_token").asText();
-        } catch (Exception e) {
-            System.err.println("Failed to parse access_token: " + e.getMessage());
-        }
-        System.out.println("accessToken = " + accessToken);
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("https://www.reddit.com/api/v1/access_token"))
+//                .header("Content-Type", "application/x-www-form-urlencoded")
+//                .header("User-Agent", "rdating/0.1 (by /u/Food-Little)")
+//                .header("Authorization", String.format("Basic %s", credentials))
+//                .POST(HttpRequest.BodyPublishers.ofString(String.format("grant_type=password&username=%s&password=%s", user, password)))
+//                .build();
+//
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        String responseBody = response.body();
+//        // Parse JSON response to extract access_token
+//        String accessToken = null;
+//        try {
+//            JsonNode jsonNode =
+//                new ObjectMapper().readTree(responseBody);
+//            accessToken = jsonNode.get("access_token").asText();
+//        } catch (Exception e) {
+//            System.err.println("Failed to parse access_token: " + e.getMessage());
+//        }
+//        System.out.println("accessToken = " + accessToken);
 
         //after is this value: jq '.data.children[0].data.name'
 //        HttpRequest userRequest = HttpRequest.newBuilder()
@@ -57,39 +57,52 @@ public class Test {
 
 
 //        HttpRequest userRequest = HttpRequest.newBuilder()
-//                .uri(URI.create("https://oauth.reddit.com/r/fishcommunity12345/comments/1m4afk7.json?after=n42wn39limit=1"))
+//                .uri(URI.create("https://oauth.reddit.com/r/fishcommunity12345/comments/1m4afk7.json"))
 //                .header("User-Agent", "rdating/0.1 (by /u/Food-Little)")
 //                .header("Authorization", "Bearer " + accessToken)
 //                .build();
 //
 //        response = client.send(userRequest, HttpResponse.BodyHandlers.ofString());
 //        System.out.println(response.body());
-
-//        HttpRequest userRequest = HttpRequest.newBuilder()
-//                .uri(URI.create("https://oauth.reddit.com/user/vonrosen2020/upvoted"))
-//                .header("User-Agent", "rdating/0.1 (by /u/Food-Little)")
-//                .header("Authorization", "Bearer " + accessToken)
-//                .build();
-//
-//        response = client.send(userRequest, HttpResponse.BodyHandlers.ofString());
-//        System.out.println(response.body());
-//
 
         HttpRequest userRequest = HttpRequest.newBuilder()
-                .uri(URI.create("https://oauth.reddit.com/api/compose"))
-                .header("Content-Type", "application/x-www-form-urlencoded")
+                .uri(URI.create("https://oauth.reddit.com/user/vonrosen2020/upvoted"))
                 .header("User-Agent", "rdating/0.1 (by /u/Food-Little)")
-                .header("Authorization", "Bearer " + accessToken)
-                .POST(HttpRequest.BodyPublishers.ofString(
-                        "api_type=json" +
-                        "&from_sr=fishcommunity12345" +
-                        "&subject=Test%20Message" +
-                        "&text=You matched!" +
-                        "&to=vonrosen2020"))
+                .header("Authorization", "Bearer token")
                 .build();
 
-        response = client.send(userRequest, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(userRequest, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+//
+
+//        HttpRequest userRequest = HttpRequest.newBuilder()
+//                .uri(URI.create("https://oauth.reddit.com/api/compose"))
+//                .header("Content-Type", "application/x-www-form-urlencoded")
+//                .header("User-Agent", "rdating/0.1 (by /u/Food-Little)")
+//                .header("Authorization", "Bearer " + accessToken)
+//                .POST(HttpRequest.BodyPublishers.ofString(
+//                        "api_type=json" +
+//                        "&from_sr=fishcommunity12345" +
+//                        "&subject=Test%20Message" +
+//                        "&text=You matched!" +
+//                        "&to=vonrosen2020"))
+//                .build();
+//
+//        response = client.send(userRequest, HttpResponse.BodyHandlers.ofString());
+//        System.out.println(response.body());
+
+        //use code to get access token for user (after user has granted access)
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("https://www.reddit.com/api/v1/access_token"))
+//                .header("Content-Type", "application/x-www-form-urlencoded")
+//                .header("User-Agent", "rdating/0.1 (by /u/Food-Little)")
+//                .header("Authorization", String.format("Basic %s", credentials))
+//                .POST(HttpRequest.BodyPublishers.ofString("grant_type=authorization_code&code=code&redirect_uri=https://github.com/vonrosen/reddit-dating"))
+//                .build();
+//
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        System.out.println(response.body());
+
     }
 
 }
